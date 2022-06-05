a73ff236061b70f6de2bee58e56d88ee38e2c09c
mohamed kamal <mkahf5103@gmail.com>
2022-06-06 02:26:53 +0400

    change project to mvc

--- web/Startup.cs
+++ web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,7 +24,7 @@ namespace web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,8 +50,14 @@ namespace web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    "DefaultRoute",
+                    "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
+
+      
+        }
     }
-}
+
