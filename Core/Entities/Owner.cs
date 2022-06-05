ab8ee56a31b90a88ba1168d3d102540cb0f22e7f
mohamed kamal <mkahf5103@gmail.com>
2022-06-06 01:57:17 +0400

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Owner:EntityBase
+    {
+        public string MyProperty { get; set; }
+        public string JobTitle { get; set; }
+        public string profile { get; set; }
+        public Address Address { get; set; }
+    }
+}
