7594a78b79a402c128d12fac8b247cd2c39489e7
mohamed kamal <mkahf5103@gmail.com>
2022-06-07 02:49:17 +0400

    add unitofwork

---
+++ Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> table = null;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+           _context = context;
+            table=_context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            table.Add(entity);
+        }
+
+        public void Delete(object id)
+        {
+            T existing=GetTById(id);
+           table.Remove(existing);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetTById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            //T existing = GetTById(entity);
+            //table.Update(existing);
+            table.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;    
+        }
+    }
+}
---
+++ web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using System.Collections.Generic;
+
+namespace web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Owner Owner { get; set; }
+        public List<PrtofolioItem> PrtofolioItems { get; set; }
+    }
+}
