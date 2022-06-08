309500a51489205fe6e967b3492289e291b6ad20
mohamed kamal <mkahf5103@gmail.com>
2022-06-08 19:16:29 +0400

    usind unit of work in prtoflio contrpller

--- web/Controllers/PrtoflioItemsController.cs
+++ web/Controllers/PrtoflioItemsController.cs
@@ -1,24 +1,31 @@
 ﻿using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.IO;
 using System.Linq;
+using web.ViewModels;
 
 namespace web.Controllers
 {
     public class PrtoflioItemsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork<PrtofolioItem> _prtoflio;
+        private readonly IHostingEnvironment _hosting;
 
-        public PrtoflioItemsController(ApplicationDbContext context)
+        public PrtoflioItemsController(IUnitOfWork<PrtofolioItem> prtoflio
+            ,IHostingEnvironment hosting)
         {
-         _context = context;
+           _prtoflio = prtoflio;
+            _hosting = hosting;
         }
         // GET: PrtoflioItemsController
         public ActionResult Index()
         {
-            return View(_context.PrtofolioItems.ToList());
+            return View(_prtoflio.Entity.GetAll());
         }
 
         // GET: PrtoflioItemsController/Details/5
@@ -28,7 +35,7 @@ namespace web.Controllers
             {
                 return NotFound();
             }
-            var prttoflio=_context.PrtofolioItems.Find(id);
+            var prttoflio=_prtoflio.Entity.GetTById(id);
             if (prttoflio == null)
             {
                 return NotFound();
@@ -45,12 +52,24 @@ namespace web.Controllers
         // POST: PrtoflioItemsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(PrtofolioItem model)
+        public ActionResult Create(PrtoflioViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.PrtofolioItems.Add(model);
-                _context.SaveChanges();
+                if(model.File!= null)
+                {
+                    string Uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                    string FullPath = Path.Combine(Uploads, model.File.FileName);
+                    model.File.CopyTo(new FileStream(FullPath, FileMode.Create));
+                }
+                PrtofolioItem prtofolioItem = new PrtofolioItem
+                {
+                        Description = model.Description,    
+                        ProjectName=model.ProjectName,
+                        ImageUrl=model.ImageUrl
+                };
+                _prtoflio.Entity.Add(prtofolioItem);
+                _prtoflio.Save();
                 return RedirectToAction(nameof(Index));
             }
             return View(model);
@@ -63,7 +82,60 @@ namespace web.Controllers
             {
                 return NotFound();
             }
-            var prttoflio = _context.PrtofolioItems.Find(id);
+            var prttoflio =_prtoflio.Entity.GetTById(id);
+            if (prttoflio == null)
+            {
+                return NotFound();
+            }
+            PrtoflioViewModel prtoflioViewModel = new PrtoflioViewModel
+            {
+                Id= prttoflio.Id,
+                Description=prttoflio.Description,
+                ProjectName= prttoflio.ProjectName,
+                ImageUrl=prttoflio.ImageUrl
+            };
+            return View(prtoflioViewModel);
+        }
+
+        // POST: PrtoflioItemsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, PrtoflioViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                if (model.File != null)
+                {
+                    string Uploads = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                    string FullPath = Path.Combine(Uploads, model.File.FileName);
+                    model.File.CopyTo(new FileStream(FullPath, FileMode.Create));
+                }
+                PrtofolioItem prtofolioItem = new PrtofolioItem
+                {
+                    Description = model.Description,
+                    ProjectName = model.ProjectName,
+                    ImageUrl = model.ImageUrl
+                };
+                _prtoflio.Entity.Update(prtofolioItem);
+                _prtoflio.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model); 
+
+        }
+
+        // GET: PrtoflioItemsController/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var prttoflio = _prtoflio.Entity.GetTById(id);
             if (prttoflio == null)
             {
                 return NotFound();
@@ -71,40 +143,14 @@ namespace web.Controllers
             return View(prttoflio);
         }
 
-        // POST: PrtoflioItemsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: PrtoflioItemsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
         // POST: PrtoflioItemsController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(Guid id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            _prtoflio.Entity.Delete(id);
+            _prtoflio.Save();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
